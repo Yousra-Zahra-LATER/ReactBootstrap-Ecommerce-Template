0240e6750acfc1e584c7e0ca240de98d10cbd749
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-04-13 19:04:06 +0200

    "last commit"

--- src/component/Home/About.js
+++ src/component/Home/About.js
@@ -5,7 +5,7 @@ import Col from 'react-bootstrap/Col';
 import beef from '../../assets/beef.jpg';
 import aboutt from '../../assets/aboutt.png';
 import "./About.css";
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faPlay } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function About() {
   return (
@@ -48,11 +48,14 @@ export default function About() {
           <Container>
           <Row>
           <Col md={12}>
-            <h2>Good food choices are good investments.</h2>
+            <h2>When a man's stomach is full it makes  <br/>
+                     difference whether he is rich or poor. </h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio  <br/>
+                   finibus bibendum in sit amet leo. Mauris feugiat erat tellus.
+
             </p>
-              <a href='#'> Watch Our Story</a>
+              <a href='#'> <FontAwesomeIcon icon={faPlay} />  Watch Our Story</a>
           </Col>
         </Row>
             </Container>
