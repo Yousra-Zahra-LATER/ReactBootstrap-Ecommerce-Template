58ef1b2fe37d313a77e3ef754ce0d05794261f1f
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-04-13 02:31:12 +0200

    Update About.js

--- src/component/Home/About.js
+++ src/component/Home/About.js
@@ -1,28 +1,20 @@
 import React from 'react'
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import beef from '../../assets/beef.jpg';
+import "./About.css";
 export default function About() {
   return (
-    <Container className="sectionNumber" fluid='true'>
-      <Row >
-        <Col>
-        <h2>1287+</h2>
-        <h6>SAVINGS</h6>
+    <Container className="about" >
+      <Row>
+        <Col md={6} ><img src={beef} title="about" alt='About' /></Col>
+        <Col md={6} className="tol">
+        <h2>We pride ourselves on making real food from the best ingredients.</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et purus a odio finibus bibendum in sit amet leo. Mauris feugiat erat tellus.</p>
+        <button > <a href="#">Learn More </a> </button>
         </Col>
-
-        <Col>
-        <h2>5786+</h2>
-        <h6>PHOTOS</h6>
-        </Col>
-
-        <Col>
-        <h2>1440+</h2>
-        <h6>ROCKETS</h6>
-        </Col>
-        <Col>
-        <h2>7110+</h2>
-        <h6>GLOBES</h6>
-        </Col>
-      </Row>
+      </Row>   
     </Container>
   )
 }
