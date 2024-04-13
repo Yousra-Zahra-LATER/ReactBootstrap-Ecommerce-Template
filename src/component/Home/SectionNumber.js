a32a7c48b998699df3ebe11253a5ec4f7b35925a
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-04-13 02:30:36 +0200

    Update SectionNumber.js

--- src/component/Home/SectionNumber.js
+++ src/component/Home/SectionNumber.js
@@ -5,20 +5,26 @@ import Col from 'react-bootstrap/Col';
 import './SectionNumber.css';
 export default function () {
   return (
-    <Container className="about" >
+    <Container className="sectionNumber" fluid='true'>
       <Row >
-        
-        <Col>
+        <Col md={3}>
         <h2>1287+</h2>
         <h6>SAVINGS</h6>
         </Col>
 
-        <Col>
+        <Col md={3}>
         <h2>5786+</h2>
         <h6>PHOTOS</h6>
         </Col>
 
-      
+        <Col md={3}>
+        <h2>1440+</h2>
+        <h6>ROCKETS</h6>
+        </Col>
+        <Col  md={3}> 
+        <h2>7110+</h2>
+        <h6>GLOBES</h6>
+        </Col>
       </Row>
     </Container>
    
