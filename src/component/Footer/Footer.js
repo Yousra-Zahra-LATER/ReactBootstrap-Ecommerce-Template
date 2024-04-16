1b16f2789e2ffe704a503faa25b6f49372d4ed44
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-04-16 22:40:11 +0100

    "Last Commit Code of Footer"

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
+
   return (
-    <div>Footer</div>
-  )
+<footer style={{ backgroundColor: '#7c0f28', padding: '1rem 0' }} className="text-white ">  
+    <Container>
+        <Row>
+        <Col>
+  <ul className="list-unstyled d-flex justify-content-center">
+    <li className="mx-3"><a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}>Regiter</a></li>
+    <li className="mx-3"><a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}>Forum</a></li>
+    <li className="mx-3"><a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}>Affiliate</a></li>
+    <li className="mx-3"><a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}>FAQ</a></li>
+  </ul>
+</Col>
+        </Row>
+        <Row>
+        <Col>
+  <div className="d-flex justify-content-center">
+  <a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}><FontAwesomeIcon icon={faFacebook} className="m-2" /></a>
+  <a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}> <FontAwesomeIcon icon={faTwitter} className="m-2" /></a>
+  <a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}><FontAwesomeIcon icon={faInstagram} className="m-2" /></a>
+  </div>
+</Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="text-center mt-5">&copy; {new Date().getFullYear()} Infinity. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
