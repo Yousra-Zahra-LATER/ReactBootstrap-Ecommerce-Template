34c1c18b3b11cf8a7dc8b528b790979424feb3cb
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-04-19 21:25:02 +0100

    "Last Update"

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -6,30 +6,24 @@ import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg
 export default function Footer() {
 
   return (
-<footer style={{ backgroundColor: '#7c0f28', padding: '1rem 0' }} className="text-white ">  
-    <Container>
-        <Row>
-        <Col>
+<footer style={{ backgroundColor: '#7c0f28', padding: '2rem 0' }} className="text-white ">  
+    <Container >
+        <Row >
+        <Col >
   <ul className="list-unstyled d-flex justify-content-center">
     <li className="mx-3"><a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}>Regiter</a></li>
     <li className="mx-3"><a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}>Forum</a></li>
     <li className="mx-3"><a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}>Affiliate</a></li>
     <li className="mx-3"><a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}>FAQ</a></li>
   </ul>
-</Col>
-        </Row>
-        <Row>
-        <Col>
+
   <div className="d-flex justify-content-center">
   <a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}><FontAwesomeIcon icon={faFacebook} className="m-2" /></a>
   <a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}> <FontAwesomeIcon icon={faTwitter} className="m-2" /></a>
   <a href="#" style={{ textDecoration: 'none' , color: 'inherit'}}><FontAwesomeIcon icon={faInstagram} className="m-2" /></a>
   </div>
-</Col>
-        </Row>
-        <Row>
-          <Col>
-            <p className="text-center mt-4">&copy; {new Date().getFullYear()} Infinity. All rights reserved.</p>
+
+            <p className="text-center mt-4 mb-0" style={{ fontSize: '13px'}}>&copy; {new Date().getFullYear()} Infinity. All rights reserved.</p>
           </Col>
         </Row>
       </Container>
