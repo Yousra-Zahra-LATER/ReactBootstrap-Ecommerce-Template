d36ba99419d368eb90c63515efc181f2930b8840
Yousra Zahra LATER <yousra.later@univ-constantine2.dz>
2024-04-30 10:35:00 +0100

    " last changes"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 
 import './App.css';
 import Footer from './component/Footer/Footer';
-import AboutSection from './component/Home/AboutSection';
-import BlogSection from './component/Home/BlogSection';
-import FAQ from './component/Home/FAQ';
 import Home from './component/Home/Home';
-import Reviews from './component/Home/Reviews';
 import NAV from './component/Nav/Nav';
 
 function App() {
